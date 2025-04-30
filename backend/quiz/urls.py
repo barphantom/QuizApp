fd787cb7371c18318919f9c47c1b4c990c514d28
adrianpiecek <adrian.piecek@gmail.com>
2025-04-30 17:10:10 +0200

    list posting prototype

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -3,12 +3,10 @@ from rest_framework.routers import DefaultRouter
 from . import views
 from .views import QuizViewSet, QuestionViewSet, AnswerViewSet
 
-router = DefaultRouter()
-router.register(r'quizzes', QuizViewSet)
-router.register(r'questions', QuestionViewSet)
-router.register(r'answers', AnswerViewSet)
+quiz_list = QuizViewSet.as_view({'get': 'list'})
 
 urlpatterns = [
     path('test', views.index, name='index'),
-    path('', include(router.urls)),
+    path('quiz/', quiz_list, name='quiz-list'),
+    path("quiz-test/", views.quiz_list, name="quiz-test"),
 ]
--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -4,20 +4,37 @@ from django.template import loader
 from rest_framework import viewsets
 from .models import Quiz, Question, Answer
 from .serializers import QuizSerializer, QuestionSerializer, AnswerSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 def index(request):
     template = loader.get_template('test.html')
     return HttpResponse(template.render())
 
+@api_view(['POST'])
+def quiz_list(request):
+    if request.method == 'POST':
+        data = request.data
+        name = data.get('name', 'bartek')
+        print(name)
+        return Response({"message": "Quiz list view"})
+    return Response({"error": "Invalid request method"}, status=400)
 
 class QuizViewSet(viewsets.ModelViewSet):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
+    permission_classes = [AllowAny]  # Allow any user to access this view
+    
+    def list(self, request, *args, **kwargs):
+        return HttpResponse("Hello, world! This is the quiz list view.")
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    permission_classes = [AllowAny]  # Allow any user to access this view
 
 class AnswerViewSet(viewsets.ModelViewSet):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
+    permission_classes = [AllowAny]  # Allow any user to access this view
