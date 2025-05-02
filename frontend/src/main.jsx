de8643bee583a29e8c17f2c99a19ab0758f60896
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-02 13:04:19 +0200

    Changes in app routing structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,32 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
-import NotFoundPage from "./NotFoundPage.jsx";
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
 
-const router = createBrowserRouter([
-    // {
-    //     path: "/",
-    //     element: <App />,
-    //     children: [
-    //         { index: true, element: <Home /> },
-    //         { path: 'loginn', element: <Login /> },
-    //         { path: 'registerr', element: <Register /> },
-    //     ],
-    // },
-    {path: "/", element: <App />},
-    {path: "/login", element: <Login />},
-    {path: "/register", element: <Register />},
-    {path: "*", element: <NotFoundPage />},
-]);
 
-// konfiguracja rutera
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
