58025fed06a1023488387b11c9803bf6e2fa9cce
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-02 13:05:13 +0200

    Change login and register pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,9 @@
 import { Link } from 'react-router-dom';
+import Form from "../components/Form.jsx";
+
 
 export default function Login() {
   return (
-    <div>
-      <h1>Logowanie</h1>
-      <form>
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Hasło" required />
-        <button type="submit">Zaloguj się</button>
-      </form>
-      <p>
-        Nie masz konta? <Link to="/register">Zarejestruj się</Link>
-      </p>
-    </div>
+      <Form route={"/api/token/"} method="login" />
   );
 }
