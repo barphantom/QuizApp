8cf894f0afb235214a66f0ead52cfe49aa72e6ba
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-05 12:46:48 +0200

    Created custom user model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,12 +1,46 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Użytkownik musi mieć adres email")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100, unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'nickname']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
 
 
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='notes')
 
     def __str__(self):
         return self.title
