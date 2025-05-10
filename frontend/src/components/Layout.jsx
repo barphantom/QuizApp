a7564fd7fcc50b843241ab983573d6ad32112410
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-10 23:56:50 +0200

    Modified layout component. Added sidebar

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,11 +1,17 @@
 import Navbar from './Navbar';
+import Sidebar from "./Sidebar.jsx";
 import styles from '../styles/Layout.module.css';
 
 export default function Layout({ children }) {
   return (
     <div className={styles.container}>
-      <Navbar />
-      <main>{children}</main>
+        <Navbar />
+        <div className={styles.contentArea}>
+            <Sidebar />
+            <main className={styles.mainContent}>
+                {children}
+            </main>
+        </div>
     </div>
   );
 }
