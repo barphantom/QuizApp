93cb6abb7db65ce91d9db4abcac14e244c71c012
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-10 23:55:24 +0200

    Created new api endpoint for GET and POST method

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import QuizListCreateView
 
 urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>", views.NoteDelete.as_view(), name="delete-note"),
+    path("quizzes/", QuizListCreateView.as_view(), name="quiz-list-create"),
 ]
 
