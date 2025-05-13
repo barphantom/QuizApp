29c42e78241394d600101edf8d5fe93c767a277d
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-13 13:19:21 +0200

    Updated QuizSerializer for updating quiz

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -64,4 +64,21 @@ class QuizSerializer(serializers.ModelSerializer):
             question = Question.objects.create(quiz=quiz, **question_data)
             for answer_data in answers_data:
                 Answer.objects.create(question=question, **answer_data)
+
         return quiz
+
+    def update(self, instance, validated_data):
+        questions_data = validated_data.pop("questions")
+
+        instance.title = validated_data.get("title", instance.title)
+        instance.save()
+
+        instance.questions.all().delete()
+
+        for question_data in questions_data:
+            answers_data = question_data.pop("answers")
+            question = Question.objects.create(quiz=instance, **question_data)
+            for answer_data in answers_data:
+                Answer.objects.create(question=question, **answer_data)
+
+        return instance
