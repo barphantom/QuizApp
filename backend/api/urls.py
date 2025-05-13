64b4c657e7bb83fff9ca20f3db8204dc816da901
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-13 13:20:32 +0200

    Added new dynamic url for GET, PUT, DELETE quiz instance api calls.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import QuizListCreateView
+from .views import QuizListCreateView, QuizDetailView
 
 urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
-    path("notes/delete/<int:pk>", views.NoteDelete.as_view(), name="delete-note"),
+    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
     path("quizzes/", QuizListCreateView.as_view(), name="quiz-list-create"),
+    path("quizzes/<int:pk>/", QuizDetailView.as_view(), name="quiz-detail"),
 ]
 
