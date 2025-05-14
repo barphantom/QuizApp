61d200bc2ef8a9da50ab814e0dc78a9ac90fa3bb
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-14 14:40:16 +0200

    Created new serializers for displaying quiz info and creating quiz results

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,8 @@
+import random, string
+
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Note, CustomUser, Answer, Question, Quiz
+from .models import Note, CustomUser, Answer, Question, Quiz, AnswerSubmission, QuizResult
 
 
 # class UserSerializer(serializers.ModelSerializer):
@@ -48,6 +50,13 @@ class QuestionSerializer(serializers.ModelSerializer):
         fields = ["text", "answers"]
 
 
+def generate_unique_code():
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+        if not Quiz.objects.filter(code=code).exists():
+            return code
+
+
 class QuizSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True)
 
@@ -58,7 +67,7 @@ class QuizSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         questions_data = validated_data.pop("questions")
-        quiz = Quiz.objects.create(**validated_data)
+        quiz = Quiz.objects.create(code=generate_unique_code(), **validated_data)
         for question_data in questions_data:
             answers_data = question_data.pop("answers")
             question = Question.objects.create(quiz=quiz, **question_data)
@@ -82,3 +91,67 @@ class QuizSerializer(serializers.ModelSerializer):
                 Answer.objects.create(question=question, **answer_data)
 
         return instance
+
+
+class PublicAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ["id", "text"]
+
+
+class PublicQuestionSerializer(serializers.ModelSerializer):
+    answers = PublicAnswerSerializer(many=True)
+
+    class Meta:
+        model = Question
+        fields = ["id", "text", "answers"]
+
+
+class PublicQuizSerializer(serializers.ModelSerializer):
+    questions = PublicQuestionSerializer(many=True)
+
+    class Meta:
+        model = Quiz
+        fields = ["id", "title", "questions"]
+
+
+class AnswerSubmissionSerializer(serializers.ModelSerializer):
+    question = serializers.PrimaryKeyRelatedField(queryset=Quiz.objects.all())
+    selected_answer = serializers.PrimaryKeyRelatedField(queryset=Answer.objects.all())
+
+    class Meta:
+        model = AnswerSubmission
+        fields = ["question", "selected_answer"]
+
+
+class QuizResultSerializer(serializers.ModelSerializer):
+    answers = AnswerSubmissionSerializer(many=True)
+    quiz = serializers.PrimaryKeyRelatedField(queryset=Quiz.objects.all())
+
+    class Meta:
+        model = QuizResult
+        fields = ["quiz", "answers"]
+
+    def create(self, validated_data):
+        answers_data = validated_data.pop("answers")
+        quiz = validated_data["quiz"]
+        user = self.context["request"].user if self.context["request"].user.is_authenticated else None
+
+        total = quiz.questions.count()
+        score = 0
+
+        result = QuizResult.objects.create(quiz=quiz, user=user, score=0, total=total)
+
+        for answer_data in answers_data:
+            question = answer_data["question"]
+            selected_answer = answer_data["selected_answer"]
+
+            AnswerSubmission.objects.create(result=result, question=question, selected_answer=selected_answer)
+
+            if selected_answer.is_correct:
+                score += 1
+
+        result.score = score
+        result.save()
+
+        return result
