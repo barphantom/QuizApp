6548e60675aaefa250bd0ff334edbe5a7ea41da9
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-15 22:27:53 +0200

    Added input for quiz code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,32 @@
 import styles from '../styles/Home.module.css';
 import logo from '../assets/logo.png';
 import { useNavigate } from 'react-router-dom';
+import {useState} from "react";
+import api from "../api.js";
 
 export default function Home() {
   const navigate = useNavigate();
+  const [code, setCode] = useState("")
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+
+      if (code.length !== 6) {
+          alert("Kod musi mieć dokładnie 6 znaków.")
+          return
+      }
+
+      api
+          .get(`/api/quizzes/join/${code}/`)
+          .then((response) => {
+              const quiz = response.data
+              navigate(`/fill-out-quiz/${code}`, { state: {quiz} })
+          })
+          .catch((error) => {
+              console.log("Wystąpił błąd przy dołączniu do quizu.", error)
+              alert("Wystąpił błąd przy dołączniu do quizu. Spróbuj jeszcze raz.")
+          })
+  }
 
   return (
     <div>
@@ -14,6 +37,15 @@ export default function Home() {
                 <button onClick={() => navigate("/register")}>Sign up</button>
               </div>
           </nav>
+        <h2>Wpisz kod aby dołączyć do quizu</h2>
+        <form onSubmit={handleSubmit}>
+            <input type="text"
+                   name="kod"
+                   value={code}
+                   onChange={(e) => setCode(e.target.value)}
+                   placeholder="kod quizu"
+                   required/>
+        </form>
     </div>
   );
 }
