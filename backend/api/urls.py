611c442f5fe643cb59f9310adf00e602e8b22f7d
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-17 21:02:16 +0200

    Created new url for fetching quiz stats

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import QuizListCreateView, QuizDetailView, JoinQuizView, SubmitQuizResultView
+from .views import QuizListCreateView, QuizDetailView, JoinQuizView, SubmitQuizResultView, QuizStatsView
 
 urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
@@ -9,5 +9,6 @@ urlpatterns = [
     path("quizzes/<int:pk>/", QuizDetailView.as_view(), name="quiz-detail"),
     path("quizzes/join/<str:code>/", JoinQuizView.as_view(), name="join-quiz"),
     path("quizzes/submit-quiz/", SubmitQuizResultView.as_view(), name="submit-quiz"),
+    path("quizzes/get-quiz-stats/<int:quiz_id>/", QuizStatsView.as_view(), name="get-quiz-stats"),
 ]
 
