1e1664615c946cfac6efb1699e841db8cfd29ead
Bartek Dobrzanski <baarti02@gmail.com>
2025-05-19 16:24:05 +0200

    Changed layout and added styles

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,13 +2,13 @@ import Navbar from './Navbar';
 import Sidebar from "./Sidebar.jsx";
 import styles from '../styles/Layout.module.css';
 
-export default function Layout({ children }) {
+export default function Layout({ children, withSidebar = true }) {
   return (
     <div className={styles.container}>
         <Navbar />
         <div className={styles.contentArea}>
-            <Sidebar />
-            <main className={styles.mainContent}>
+            {withSidebar && <Sidebar />}
+            <main className={`${styles.mainContent} ${!withSidebar ? styles.fullWidth : ''}`}>
                 {children}
             </main>
         </div>
